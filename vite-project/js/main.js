b4355f9a80a9fc185999b28b1e5ccf494b91e7a5
Seebso <124058253+Seebso@users.noreply.github.com>
2023-11-22 12:24:34 -0500

    adwfgesgs

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,6 @@
 import "../styles/style.css";
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from '../counter.js'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href=https://vitejs.dev/guide/features.html" target="_blank>Documentation</a>
-
+`;
