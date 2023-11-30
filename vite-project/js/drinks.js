cc0e6dcdd994d7f5a2d306a7bc911fa5c4bdb37c
Seebso <124058253+Seebso@users.noreply.github.com>
2023-11-30 13:12:47 -0500

    asdw

--- vite-project/js/drinks.js
+++ vite-project/js/drinks.js
@@ -1,14 +1,22 @@
 const drinks = [
     {
         name: "water",
-        type: "still"
+        type: "still",
+        price: "1",
+        img: "https://5.imimg.com/data5/SELLER/Default/2021/3/VC/TC/QO/70500095/bibo-300-ml-packaged-drinking-water-1000x1000.jpg"
     },
     {
         name: "sparkling water",
-        type: "carbonated"
+        type: "carbonated",
+        price: "1.99",
+        img: "https://th.bing.com/th?id=OPHS.swKUAw9Jk4zXOg474C474&w=220&h=220&o=5&pid=21.1"
     },
-    
+    {
+        name: "Milk Bubble Tea",
+        type: "still",
+        price: "6",
+        img: "https://th.bing.com/th/id/OIP.ne_z_5ABA7e8-nlIgiMsygHaL9?w=196&h=317&c=7&r=0&o=5&pid=1.7"
+    },
 
-
-
-]
+];
+export { drinks }
