72c8c7b2e690f33f8f5a37b30b90eb4377df6429
Seebso <124058253+Seebso@users.noreply.github.com>
2023-12-04 00:50:05 -0500

    almost1

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,12 +1,55 @@
 import "../styles/style.css";
 import { drinks } from "./drinks";
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <p> fwesinju </p>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
 const DOMSelectors = {
   card: document.querySelector("#con"),
-}
-console.log(drinks)
+  button: document.getElementById("btn"),
+  but: document.getElementById("but"),
+  idk: document.querySelector("idk"),
+};
+document.querySelector(".light").addEventListener("click", function () {
+  if(document.body.classList.contains("light")){
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else{
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+  if(document.card.classList.contains("light")){
+    document.card.classList.add("dark");
+    document.card.classList.remove("light");
+  } else{
+    document.card.classList.add("light");
+    document.card.classList.remove("dark");
+  }
+  if(document.idk.classList.contains("light")){
+    document.idk.classList.add("dark");
+    document.idk.classList.remove("light");
+  } else{
+    document.idk.classList.add("light");
+    document.idk.classList.remove("dark");
+  }
+});
+
+const type = drinks.filter((drinks) => drinks.type != "still");
+console.log(type)
 drinks.forEach((drink) => DOMSelectors.card.insertAdjacentHTML("beforeend",`<div class="card"><h1> ${drink.name}</h1>img src="drink.img"</div>`))
+DOMSelectors.button.addEventListener("click", function () {
+  const type = drinks.filter((drinks) => drinks.type != "still");
+  DOMSelectors.card.innerHTML = "";
+  type.forEach((drink) => DOMSelectors.card.insertAdjacentHTML("beforeend",`<div class="card"><h1> ${drink.name}</h1>img src="drink.img"</div>`))
+console.log(type)
+})
+DOMSelectors.but.addEventListener("click", function () {
+  const type = drinks.filter((drinks) => drinks.type != "carbonated");
+  DOMSelectors.card.innerHTML = "";
+  type.forEach((drink) => DOMSelectors.card.insertAdjacentHTML("beforeend",`<div class="card"><h1> ${drink.name}</h1>img src="drink.img"</div>`))
+})
+
+/* function remove() {
+    let buto = document.querySelectorAll(".btn");
+    buto.forEach((btn) => 
+        btn.addEventListener("click",  (clic) => {
+            clic.currentTarget.parentNode.remove();
+        })
+    );
+} */
